8c589f3f680b5f09a814231674cf3f906edc7459
Roderick <roderick.martina.p@gmail.com>
2021-02-09 21:01:56 +0100

    updated dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react'
-import axios from 'axios'
 import Layout from "@/Shared/Layout";
+import NestedLayout from "@/Shared/NestedLayout";
+import {IDefaultProps} from '@/Interfaces'
 
-const Dashboard = () => {
-    const logout = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault()
-        axios.post('/logout')
-            .then(() => {
-                window.location.href = '/'
-            })
-    }
+const Dashboard = ({route_name, auth}: IDefaultProps) => {
     return (
-        <div className={``}>
-            <div>dit is de dashboard</div>
-            <button
-                className={`border bg-blue-500 text-white py-3 px-4 rounded-lg`}
-                onClick={logout}
-            >
-                logout
-            </button>
-        </div>
+        <NestedLayout name={auth.user.name} route_name={route_name}>
+            <h1 className="text-lg leading-6 font-medium text-cool-gray-900">Dashboard</h1>
+            <div className="py-4">
+                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            </div>
+        </NestedLayout>
     )
 }
 
-Dashboard.layout = (page: React.ReactNode)=> <Layout children={page} />
+Dashboard.layout = (page: React.ReactNode) => <Layout children={page}/>
 
 export default Dashboard
