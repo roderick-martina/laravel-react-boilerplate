112553b860e0d0c6d80039f552372a1f4418c0b5
Roderick <roderick.martina.p@gmail.com>
2021-02-09 21:03:33 +0100

    added fullname attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
