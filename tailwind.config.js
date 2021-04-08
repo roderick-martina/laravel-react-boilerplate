84af9efb123075b11589f1330bcaeac8bcb60069
Roderick <roderick.martina.p@gmail.com>
2021-04-08 20:06:17 +0200

    added tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,32 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        fontFamily: {
-            sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    mode: 'jit',
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.{js,jsx,ts,tsx}',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                black: '#181818',
+                'black-light': '#333',
+                primary: '#07f',
+                'primary-lighter': '#0295FF',
+                'primary-dark': '#04f'
+            },
         },
-        colors: {
-            black: '#181818',
-            'black-light': '#333',
-            primary: '#07f',
-            'primary-lighter': '#0295FF',
-            'primary-dark': '#04f'
-        },
-        // backgroundColor: {
-        //     primary: '#07f',
-        //     'primary-lighter': '#0295FF',
-        // },
-        // textColor: {
-        //     primary: '#07f',
-        //     'primary-lighter': '##0295FF',
-        // },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-      require('@tailwindcss/line-clamp'),
-  ],
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+    ],
 }
