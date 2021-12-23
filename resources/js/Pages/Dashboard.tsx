96aa93d11fc89e6fee1cfec6308345ccb6215eaf
Roderick <roderick.martina.p@gmail.com>
2021-12-23 11:38:29 +0100

    updated tailwind config

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -8,7 +8,9 @@ const Dashboard = ({route_name, auth}: IDefaultProps) => {
         <NestedLayout name={auth.user.name} route_name={route_name}>
             <h1 className="text-lg leading-6 font-medium text-cool-gray-900">Dashboard</h1>
             <div className="py-4">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                    {/*<button className="h-auto w-48 text-center font-medium text-white  bg-blue-300 ml-20 mt-20 p-4 rounded-md">dit is een test</button>*/}
+                </div>
             </div>
         </NestedLayout>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-    mode: 'jit',
-    purge: [
+    content: [
         './resources/**/*.blade.php',
         './resources/**/*.{js,jsx,ts,tsx}',
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             fontFamily: {
@@ -19,9 +17,6 @@ module.exports = {
                 'primary-dark': '#04f'
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [
         require('@tailwindcss/typography'),
